a5dc64f6dbead085004d94d09f52961eba41d55e
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-12-16 12:34:06 -0300

    feat: adds class validations to src/cats/dto/create-cat.dto.ts

--- src/cats/dto/create-cat.dto.ts
+++ src/cats/dto/create-cat.dto.ts
@@ -1,1 +1,26 @@
-export class CreateCatDto {}
+import {
+  IsInt,
+  IsNotEmpty,
+  IsString,
+  IsPositive,
+  MaxLength,
+  MinLength,
+  IsOptional,
+} from 'class-validator';
+
+export class CreateCatDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  @MaxLength(30)
+  name: string;
+
+  @IsNotEmpty()
+  @IsInt()
+  @IsPositive()
+  age: number;
+
+  @IsString()
+  @IsOptional()
+  breed?: string;
+}
