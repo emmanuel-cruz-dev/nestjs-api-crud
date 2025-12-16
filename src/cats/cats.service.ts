6c996d7ccdba34715f92c3078c27d0c4cb8cd018
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-12-16 13:00:25 -0300

    feat: updates all methods in CatsService class in src/cats/cats.service.ts

--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -1,26 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateCatDto } from './dto/create-cat.dto';
 import { UpdateCatDto } from './dto/update-cat.dto';
+import { Cat } from './entities/cat.entity';
 
 @Injectable()
 export class CatsService {
+  constructor(
+    @InjectRepository(Cat)
+    private readonly catRepository: Repository<Cat>,
+  ) {}
+
   create(createCatDto: CreateCatDto) {
-    return 'This action adds a new cat';
+    const cat = this.catRepository.create(createCatDto);
+    return this.catRepository.save(cat);
   }
 
   findAll() {
-    return `This action returns all cats`;
+    return this.catRepository.find();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} cat`;
+    return this.catRepository.findOneBy({ id });
   }
 
   update(id: number, updateCatDto: UpdateCatDto) {
-    return `This action updates a #${id} cat`;
+    return this.catRepository.update(id, updateCatDto);
   }
 
   remove(id: number) {
-    return `This action removes a #${id} cat`;
+    return this.catRepository.softDelete({ id });
   }
 }
