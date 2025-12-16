e40d2f0386434d871344fc3562fa6cbf2e00503b
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-12-16 11:59:07 -0300

    feat: adds ConfigModule and environment variables to src/app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CatsModule } from './cats/cats.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
     CatsModule,
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'test',
+      type: process.env.DB_MYSQL_TYPE as 'mysql',
+      host: process.env.DB_MYSQL_HOST || 'localhost',
+      port: parseInt(process.env.DB_MYSQL_PORT!) || 3306,
+      username: process.env.DB_MYSQL_USERNAME || 'root',
+      password: process.env.DB_MYSQL_PASSWORD || 'root',
+      database: process.env.DB_MYSQL_DATABASE || 'test',
       autoLoadEntities: true,
-      synchronize: true,
+      synchronize: process.env.DB_MYSQL_SYNCHRONIZE === 'true',
     }),
   ],
   controllers: [],
