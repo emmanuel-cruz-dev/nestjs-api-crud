56ff67a35b131f4e2219286be4c9b122b9d5aeda
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-12-17 08:25:08 -0300

    feat: adds UsersModule and AuthModule to src/app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CatsModule } from './cats/cats.module';
 import { BreedsModule } from './breeds/breeds.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -22,6 +24,8 @@ import { BreedsModule } from './breeds/breeds.module';
     }),
     CatsModule,
     BreedsModule,
+    UsersModule,
+    AuthModule,
   ],
   controllers: [],
   providers: [],
