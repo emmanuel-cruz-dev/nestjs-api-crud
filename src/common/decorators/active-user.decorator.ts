42e43dc5e91d30b1a319cf567fd2ef8a3b0782c9
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-12-18 15:59:11 -0300

    feat: adds types to request in src/common/decorators/active-user.decorator.ts

--- src/common/decorators/active-user.decorator.ts
+++ src/common/decorators/active-user.decorator.ts
@@ -1,8 +1,11 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { UserActiveInterface } from '../interfaces/user-active.interface';
 
 export const ActiveUser = createParamDecorator(
   (data: unknown, ctx: ExecutionContext) => {
-    const request = ctx.switchToHttp().getRequest();
+    const request = ctx
+      .switchToHttp()
+      .getRequest<{ user?: UserActiveInterface } & Request>();
     return request.user;
   },
 );
