5f598e9bdc8a0bfd5df99ed7927a74c3008ff971
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-12-19 07:33:20 -0300

    feat: adds swagger module configuration to src/main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -15,6 +16,15 @@ async function bootstrap() {
         transform: true,
       }),
     );
+
+    const config = new DocumentBuilder()
+      .setTitle('NestJS CRUD API')
+      .setDescription('NestJS CRUD API')
+      .setVersion('1.0')
+      .addBearerAuth()
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api/docs', app, document);
 
     await app.listen(process.env.PORT ?? 3000);
   } catch (error) {
